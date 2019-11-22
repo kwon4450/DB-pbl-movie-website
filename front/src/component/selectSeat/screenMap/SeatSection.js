3e13cf04e3d65a75aef6105ba6c93e836b2b65ca
Unknown <wogns3623@gmail.com>
2019-11-23 01:38:05 +0900

    Modify ScreenMap component
    Add row's alphabet name

--- front/src/component/selectSeat/screenMap/SeatSection.js
+++ front/src/component/selectSeat/screenMap/SeatSection.js
@@ -6,7 +6,7 @@ class SeatSection extends Component{
   static defaultProps = {
     isDummy: false
   }
-  
+
   renderSeat = () => {
     let jsx = [];
     let tmp = [];
@@ -14,10 +14,17 @@ class SeatSection extends Component{
       row:this.props.seats[0].row,
       col:this.props.startCol-1
     };
+    if(this.props.startCol == 1){
+      tmp.push((<td className="rowName">{String.fromCharCode(bSeat.row+64)}</td>));
+    }
+    
     for(const seat of this.props.seats) {
       if(bSeat.row < seat.row) {
         jsx.push(<tr className='SeatRow' key={bSeat.row}>{tmp}</tr>);
         tmp = [];
+        if(this.props.startCol == 1){
+          tmp.push((<td className="rowName">{String.fromCharCode(bSeat.row+65)}</td>));
+        }
       }
       if(seat.col-bSeat.col > 1) {
         for(let i=bSeat.col+1; i<seat.col; i++) {
