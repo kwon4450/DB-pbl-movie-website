b4aff53ed7e61677f9c37b01e9ed41261a62e420
wogns3623 <wogns3623@gmail.com>
2019-11-27 04:44:43 +0900

    Modify header
    헤더에 메뉴와 로고 추가하고 레이아웃을 잡음

--- front/src/component/header/index.js
+++ front/src/component/header/index.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import DropdownMenu from 'component/menu/DropdownMemu';
+import Menu, { MenuItem } from 'component/menu/Menu';
+import 'style/menu.css';
+import 'style/header.css';
 
 class Header extends Component {
   render() {
     return (
       <header>
-        <h1>DBV</h1>
+        <div className='logo'>
+          <Link to='/'><h1>DBV</h1></Link>
+        </div>
+        
+        <Menu className='user-menu'>
+          <MenuItem><Link to='/login'>로그인</Link></MenuItem>
+          <MenuItem><Link to='/sign-up'>회원가입</Link></MenuItem>
+        </Menu>
+
+        <Menu className='nav-menu'>
+          <DropdownMenu title='영화'>
+            <Link to='/movies'>무비 차트</Link>
+            <Link to='/movies/finder'>무비 파인더</Link>
+          </DropdownMenu>
+
+          <DropdownMenu title='예매'>
+            <Link to='/ticketing/time-table'>상영 시간표</Link>
+            <Link to='/ticketing/fast'>빠른 예매</Link>
+          </DropdownMenu>
+
+          <MenuItem>
+            <Link to='/theater'>영화관</Link>
+          </MenuItem>
+        </Menu>
       </header>
     );
   }
---
+++ front/src/component/menu/DropdownMemu.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import 'style/dropdownMenu.css';
+import Menu, { MenuItem } from './Menu';
+
+class DropdownMenu extends Component{
+  render() {
+    console.log(this.props.children);
+    return(
+      <div className={'Dropdown'+(this.props.className!==undefined?' '+this.props.className:'')}>
+        <div className='btn'><span>{this.props.title}</span></div>
+        <Menu className='content'>
+          {this.props.children.map((item, index) => (
+            <MenuItem key={index}>{item}</MenuItem>
+          ))}
+        </Menu>
+      </div>
+    );
+  }
+}
+
+export default DropdownMenu;
