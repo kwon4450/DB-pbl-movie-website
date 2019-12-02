c198962e81f4d808c1aee194210019fca4e77a5f
jungin22 <leejungin199@naver.com>
2019-12-02 19:02:23 +0900

    add MovieSlide
    MovieSlide and images add
    change 3 css (movieslide, review,main homepage)

--- front/src/pages/movies/pages/Review.js
+++ front/src/pages/movies/pages/Review.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 // import ReviewWrite from "component/review_under/reviewwrite";
 import ReviewData from "component/review_under/reviewdata";
+import Slideshow2 from "practice/Slide2";
 
 class Review extends Component {
   render() {
     return (
       <div>
+        <Slideshow2></Slideshow2>
         <ReviewData></ReviewData>
+        
       </div>
     );
   }
