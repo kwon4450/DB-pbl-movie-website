fc42618e94225a24c987be8b353348558ccd5237
minoojjang <43629410+minoojjang@users.noreply.github.com>
2019-12-04 22:10:31 +0900

    Update insertTheater.sql

--- back/db/sql/insert/insertTheater.sql
+++ back/db/sql/insert/insertTheater.sql
@@ -1,4 +1,4 @@
 insert into theater(citycode,name,address,tle) values('0211','안산','~','1');
 
-insert into screen(id,name) values((select id from theater where name='안산'), '1관');
+insert into screen(theater_id,name) values((select id from theater where name='안산'), '1관');
 
