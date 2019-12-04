5036bfe0bebc6d07566d1fb29d3893febb0885c5
minoojjang <43629410+minoojjang@users.noreply.github.com>
2019-12-04 22:09:23 +0900

    Update insertTheater.sql

--- back/db/sql/insert/insertTheater.sql
+++ back/db/sql/insert/insertTheater.sql
@@ -1,6 +1,4 @@
-insert into theater values('안산');
+insert into theater(citycode,name,address,tle) values('0211','안산','~','1');
 
-insert into theateraddress values(15462, '광덕대로', 194, '고잔동, NC백화점 A관 6층', '경기도', '안산시', '단원구');
-
-insert into screen values((select id from theater where name='안산'), '1관');
+insert into screen(id,name) values((select id from theater where name='안산'), '1관');
 
