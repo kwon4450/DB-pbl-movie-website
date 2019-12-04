c26105ef4c3fc7cda48f1b195f35d718400b27f2
minoojjang <43629410+minoojjang@users.noreply.github.com>
2019-12-04 22:15:50 +0900

    Update insertSeat.sql

--- back/db/sql/insert/insertSeat.sql
+++ back/db/sql/insert/insertSeat.sql
@@ -1,6 +1,3 @@
-insert into theater(id, name) values(theaterAI.nextval, '안산');
-
-insert into theateraddress(theater_id, postal, street, street_num, detail, state, city, district) values(theateraddressAI.nextval, 15462, '광덕대로', 194, '고잔동, NC백화점 A관 6층', '경기도', '안산시', '단원구');
 
 -- 1관, 7관 --
 insert into screen(id, theater_id, name) values(screenAI.nextval, (select id from theater where name='안산'), '1관');
