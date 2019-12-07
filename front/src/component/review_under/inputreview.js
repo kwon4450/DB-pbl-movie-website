0aabfbdb187cffbc897422d6b44ec1fc4ac62124
Byungseo Yun <bsyun0571@naver.com>
2019-12-07 18:47:48 +0900

    Modify review

--- front/src/component/review_under/inputreview.js
+++ front/src/component/review_under/inputreview.js
@@ -7,15 +7,16 @@ class InputReview extends Component {
       <div className="input">
         <div classname="inputreview">
           <h2 classname="h2name">평점 작성</h2>
-          <strong id="겨울왕국 2"></strong>
+          <h3>겨울 왕국2</h3>
           <div class="textbox">
             <textarea
               name="input"
               cols="70"
               rows="2"
               maxlength="280"
-              placeholder="강민우 권범수 이정인 김재훈 윤병서 강민우 권범수 이정인 김재훈 윤병서"
+              placeholder="비방 및 욕설 시 사용이 정지될 수 있습니다."
             ></textarea>
+            <br></br>
           </div>
           <button submit={this.handleIncrease}>작성완료</button>
         </div>
