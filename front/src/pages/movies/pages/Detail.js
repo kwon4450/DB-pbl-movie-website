8efd4512f6559a2a1ca5d8f702bc3517a6b7b4f3
wogns3623 <wogns3623@gmail.com>
2019-12-07 17:09:26 +0900

    Modify Detail page
    extract from movieFinder

--- front/src/pages/movies/pages/Detail.js
+++ front/src/pages/movies/pages/Detail.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
 
+import Moviedetail from ".component/moviedetail";
+
 class Detail extends Component {
+  movieInfo = [
+    {
+      title2: "겨울왕국",
+      screening: "4DX 2D",
+      releasedate: "2019.11.18",
+      rating: "81.2",
+      pd: "이정인",
+      actor: "권범수",
+      genre: "Animation"
+    }
+  ];
   render() {
-    return <div>
-      
-    </div>;
+    return (
+      <div>
+        <Moviedetail {...movieInfo}></Moviedetail>
+      </div>
+    );
   }
 }
 
--- front/src/pages/movies/pages/Finder.js
+++ front/src/pages/movies/pages/Finder.js
@@ -1,14 +1,7 @@
 import React, { Component } from "react";
-import Detailpage from 'component/moviechart/Detailpage'
 class Finder extends Component {
   render() {
-    return <div>
-      <h2>영화 상세 페이지</h2>
-      <br></br>
-      <hr/>
-      <Detailpage></Detailpage>
-      {/* 임시로 이페이지에다가 해놈 */}
-    </div>;
+    return <div>movie finder</div>;
   }
 }
 
