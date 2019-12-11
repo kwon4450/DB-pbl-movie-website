ff8d7c7fb8851a171b4e822b707bba4bc5a806ee
Byungseo Yun <bsyun0571@naver.com>
2019-12-12 01:44:54 +0900

    Modify reivews & star rating

--- front/src/component/review_under/inputreview.js
+++ front/src/component/review_under/inputreview.js
@@ -2,19 +2,33 @@ import React, { Component } from "react";
 import "./style/review.css";
 
 class InputReview extends Component {
+  state = {
+    rating: 5
+  };
+
+  handleRating = rate => {
+    this.setState({ rating: rate });
+  };
+
   render() {
+    console.log(this.state.rating);
     return (
       <div className="input">
         <div classname="inputreview">
           <h2 classname="h2name">평점 및 리뷰 작성</h2>
           <h3>겨울 왕국2</h3>
           <div className="nowrap">
-            <div className="clippeda">
-              <img src="http://gahyun.wooga.kr/main/img/testImg/star_on.png"></img>
-              <img src="http://gahyun.wooga.kr/main/img/testImg/star_on.png"></img>
-              <img src="http://gahyun.wooga.kr/main/img/testImg/star_on.png"></img>
-              <img src="http://gahyun.wooga.kr/main/img/testImg/star.png"></img>
-              <img src="http://gahyun.wooga.kr/main/img/testImg/star.png"></img>
+            <div className="clipped">
+              {[1, 2, 3, 4, 5].map((item, index) => {
+                return (
+                  <img
+                    src={`http://gahyun.wooga.kr/main/img/testImg/star${
+                      this.state.rating >= item ? "_on" : ""
+                    }.png`}
+                    onClick={() => this.handleRating(item)}
+                  ></img>
+                );
+              })}
             </div>
           </div>
           <div class="textbox">
