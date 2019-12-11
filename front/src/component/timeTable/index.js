38bbe9b29d74caadd6a1bcd141803226799f08b8
BS <kwon4450@gmail.com>
2019-12-12 04:34:39 +0900

    favaritetheater issue fix

--- front/src/component/timeTable/index.js
+++ front/src/component/timeTable/index.js
@@ -49,6 +49,7 @@ class TimeTable extends Component {
   };
 
   getTimeTable(selectedTheater, index) {
+    console.log(selectedTheater);
     this.setState({
       timeTableList: this.state.timeTableList.map((item, i) => {
         if (i === index) return null;
