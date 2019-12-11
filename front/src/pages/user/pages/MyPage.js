ccdd614d1dbe1efa57e69d6dfe197e236ee0da8c
wogns3623 <wogns3623@gmail.com>
2019-12-12 08:04:09 +0900

    fix

--- front/src/pages/user/pages/MyPage.js
+++ front/src/pages/user/pages/MyPage.js
@@ -3,12 +3,33 @@ import Mypage from "component/mypage/mypage";
 import axios from "axios";
 
 class MyPage extends Component {
+  state = {
+    data: null
+  };
   componentDidMount() {
-    axios.get("/api/user/mypage");
+    axios
+      .get("/api/user/mypage")
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          data: res.data
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({
+          data: false
+        });
+      });
   }
   render() {
-    console.log("go to mypage", this.props);
-    return <Mypage {...this.props}></Mypage>;
+    if (this.state.data === null) {
+      return <h2>Loading...</h2>;
+    } else if (typeof this.state.data === "object") {
+      return <Mypage {...this.props}></Mypage>;
+    } else {
+      return <h2>Fail to loading Mypage :(</h2>;
+    }
   }
 }
 
